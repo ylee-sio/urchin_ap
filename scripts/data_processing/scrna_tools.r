fc190629362c252803f1c35473a8a0a81c5a23b3
ylee-sio <byl006@ucsd.edu>
2021-02-07 09:22:07 -0800

    added subcluster_suite in scrna_tools.r

--- scripts/data_processing/scrna_tools.r
+++ scripts/data_processing/scrna_tools.r
@@ -15,3 +15,43 @@ get_cells_with_features = function(seurat_obj, features, thresh){
   return(list_of_lists_of_cells_with_feature)
   
 }
+
+# wrapper function for preparing for clustering cells with options for clustering strategies PCA, ICA, and UMAP
+subcluster_suite = function(seurat_object, res, ndims, strat, jackstraw, coord_strat){
+  
+  tic()
+  seurat_object <- NormalizeData(seurat_object)
+  seurat_object <- FindVariableFeatures(seurat_object, nfeatures=2000)
+  seurat_object <- ScaleData(seurat_object)
+  
+  if (coord_strat == "pca"){
+    
+    seurat_object <- RunPCA(seurat_object, verbose = FALSE)
+    
+  }
+  
+  if (coord_strat == "ica"){
+    
+    seurat_object <- RunICA(seurat_object, verbose = FALSE, ica.function = "icaimax")
+    
+  }
+  seurat_object <- FindNeighbors(seurat_object, dims = ndims)
+  seurat_object <- FindClusters(seurat_object, resolution = res, verbose = FALSE, algorithm = 3)
+
+  if (jackstraw == T){
+  seurat_object = JackStraw(seurat_object, num.replicate = 200)
+  seurat_object = ScoreJackStraw(seurat_object, dims = 1:20)
+  }
+  
+  if (strat == "umap"){
+    seurat_object <- RunUMAP(seurat_object, dims = ndims, n.epochs = 500, n.neighbors = 5L, min.dist = 0.05)
+  } else {
+    seurat_object <- RunTSNE(seurat_object, dims = ndims)
+  }
+  
+  toc()
+  print("****** Job finished. ******")
+  
+  return(seurat_object)
+  
+}
