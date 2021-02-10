7352ab65fb42fef4f2bf1393e3e6a9a852923cd6
ylee-sio <byl006@ucsd.edu>
2021-02-10 13:22:09 -0800

    added DotPlot wrapper for viewability and easy communication

--- scripts/data_processing/plotting_wrappers.r
+++ scripts/data_processing/plotting_wrappers.r
@@ -1,9 +1,12 @@
+library(tidyverse)
+library(Seurat)
+library(ggplotly)
+
 output_path = "~/Projects/urchin_ap/output/"
 
 # parallel plotting of DimPlots
 # requires lists of seurat_objs
-
-plot_multiple_dp = function(cluster_list, file_name){
+plot_multiple_dimplots = function(cluster_list, file_name){
 
 # creates directory for each output
   output_dir_name = paste0(output_path, file_name, "/")
@@ -20,3 +23,32 @@ plot_multiple_dp = function(cluster_list, file_name){
 
 }
 
+# generates DotPlots which have labeled features along with option for plotly interactivity and export of interactive plots  
+lab_DP = function(scrna_df, feature_df, plot_title, by_stage = F, plot_height, interactive, col.min, col.max, dot.min, cols, name){
+  
+  
+  dp = DotPlot(scrna_df, features = feature_df$GeneID, col.min = col.min, col.max = col.max, dot.min = dot.min, cols = cols) + 
+    scale_x_discrete(breaks=c(feature_df$GeneID),labels=c(feature_df$Name)) +
+    RotatedAxis() +
+    ggtitle(as.character(plot_title)) + 
+    coord_flip()
+  
+    if (interactive == T){
+      
+      dp = ggplotly(dp, height = plot_height)
+    }
+    if (by_stage == T){
+    # in order to create dotplots by stage, scrna_df must be an seurat_obj that has multiple timepoints integrated  
+      dp = DotPlot(scrna_df, features = feature_df$GeneID, group.by = "orig.ident") + 
+        scale_x_discrete(breaks=c(feature_df$GeneID),
+                         labels=c(feature_df$Name)) +
+        RotatedAxis() +
+        ggtitle(as.character(plot_title)) + 
+        coord_flip() 
+    }
+  
+  htmlwidgets::saveWidget(as_widget(dp), paste0(output_path, name,"_index.html"))
+
+  return(dp)
+
+}  
