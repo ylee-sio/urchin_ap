cc0c2a93c5b088478a3819e0d0a3ebf48097dd99
ylee-sio <byl006@ucsd.edu>
2021-02-15 14:32:21 -0800

    commented out text manipulation in merge_isoform_names.r

--- scripts/data_processing/merge_isoform_names.r
+++ scripts/data_processing/merge_isoform_names.r
@@ -11,13 +11,13 @@ merge_isoform_feature_names_gene_label = function(features_path, features_df){
   plan("multisession", workers = 8)
   list_of_paralog_indices = future_map(list_of_paralog_dfs, ~(features_source$X2 %in% .x$GeneID %>% which()))
   first_paralog_index_list = map(.x = list_of_paralog_indices, .f = function(x)(x[1]))
-   merged_paralog_name_list = map(.x = list_of_paralog_dfs, .f = function(x)(unique(x$Name))) %>% unlist()
-   merged_paralog_name_list = merged_paralog_name_list %>%
-     str_remove_all(" \\[EC([:digit:]*.)*\\]") %>%
-     str_remove_all("\\/") %>%
-     str_remove_all(",") %>%
-     str_replace_all(" ", "_")
-   features_source[first_paralog_index_list %>% unlist(),]
+  # merged_paralog_name_list = map(.x = list_of_paralog_dfs, .f = function(x)(unique(x$Name))) %>% unlist()
+  # merged_paralog_name_list = merged_paralog_name_list %>%
+  #   str_remove_all(" \\[EC([:digit:]*.)*\\]") %>%
+  #   str_remove_all("\\/") %>%
+  #   str_remove_all(",") %>%
+  #   str_replace_all(" ", "_")
+  # features_source[first_paralog_index_list %>% unlist(),]
   merged_gene_key = bind_cols(features_source[unlist(first_paralog_index_list),]$X2, paste0("merged_gene_",1:length(features_source[unlist(first_paralog_index_list),]$X2)))
   names(merged_gene_key) = c("GeneID", "Name")
   features_source[unlist(first_paralog_index_list),]$X2 = paste0("merged_gene_",1:nrow(merged_gene_key))
