7610dedcb8f87e090f40e4ec7370a6750cc0d172
ylee-sio <byl006@ucsd.edu>
2021-02-23 08:29:57 -0800

    added feature to output a csv key for tracking merged isoform changes compared to unmodified datasets

--- scripts/data_processing/merge_isoform_counts.r
+++ scripts/data_processing/merge_isoform_counts.r
@@ -29,5 +29,35 @@ merge_paralog_feature_counts = function(matrix_path, features_path, features_df,
     }
   }
   print(paste0("100% of matrix operations for ", key_name, " completed."))
+  new_matrix = as(new_matrix, "dgCMatrix")
+  rownames(new_matrix) = NULL
+  new_matrix = new_matrix[-1,]
   
+  first_paralog_index_list = map(.x = list_of_paralog_indices, .f = function(x)(x[1])) %>% unlist()
+  other_paralog_index_list = map(.x = list_of_paralog_indices, .f = function(x)(x[-c(1)])) %>% unlist()
+  print("Copying summed matrix data...")
+
+  orig_matrix[first_paralog_index_list[1:1000],] = new_matrix[1:1000,]
+  orig_matrix[first_paralog_index_list[1001:2000],] = new_matrix[1001:2000,]
+  orig_matrix[first_paralog_index_list[2001:length(first_paralog_index_list)],] = new_matrix[2001:length(first_paralog_index_list),]
+
+  gene_names_for_key = map(.x = list_of_paralog_dfs, .f = function(x)(x[1,3])) %>% unlist(use.names = F)
+  gene_set_names_for_key = map(.x = list_of_paralog_dfs, .f = function(x)(x[1,4])) %>% unlist(use.names = F)
+  key = features_source[first_paralog_index_list,] %>% 
+    mutate(
+      GeneID = paste0("merged-gene-", 
+                  as.character(1:length(first_paralog_index_list))),
+      Name = gene_names_for_key,
+      GeneSet = gene_set_names_for_key
+      )
+  
+  features_source[first_paralog_index_list,]$X2 = key$GeneID
+  features_source = features_source[-other_paralog_index_list,]
+  write_tsv(features_source, paste0(path, "features.tsv.gz"), col_names = F)
+  
+  orig_matrix = orig_matrix[-other_paralog_index_list,]
+  writeMM(orig_matrix, paste0(path,"matrix.mtx.gz"))
+  write_csv(key, paste0(key_name,"_key.csv"))
+  
+}
 }
